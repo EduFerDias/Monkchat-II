b8d4adf7fe005feea5e964c8cdab394635c08328
EduFerDias <Diasdu2011@outlook.com>
2021-09-16 15:34:31 -0300

    Simulado OK

--- Simulado/simulado/src/Pages/Content/Style.js
+++ Simulado/simulado/src/Pages/Content/Style.js
@@ -9,7 +9,7 @@ const Conteudo = styled.div`
 .sidebar{
     font-family: "Roboto medium";
     background-color: #2B3031;
-    width: 356px;
+    width: 22.25em;
     height: 100vh;
     position: fixed;
 }
@@ -23,20 +23,20 @@ const Conteudo = styled.div`
 .logo{
     display: flex;
     align-items: center;
-    padding-left: 65px;
+    padding-left: 4em;
 }
 .logo img{
-    width: 175px;
+    width: 10em;
 }
 .accent{
-    height: 57px;
+    height: 3.5em;
     background-color: #262626;
 }
 .adm{
-    height: 57px;
+    height: 3.5em;
     color: white;
-    padding-left: 65px;
-    padding-right: 28px;
+    padding-left: 4em;
+    padding-right: 1.75em;
 }
 .adm{
     display: flex;
@@ -44,7 +44,7 @@ const Conteudo = styled.div`
     justify-content: space-between;
 }
 .adm img{
-    padding-top:8px;
+    padding-top:0.5em;
 }
 .students{
     display: flex;
@@ -52,21 +52,21 @@ const Conteudo = styled.div`
     align-items: center;
 
     background-color: white;
-    height: 57px;
+    height: 3.5em;
 }
 .detalhe{
     background-color: #E911C6;
-    width: 4px;
+    width: 0.25em;
     height: 100%;
-    margin-right: 65px;
+    margin-right: 4em;
 }
 .linha{
     background-color: #986CDF;
-    border-radius: 20px;
-    margin-right: 14px;
+    border-radius: 1.25em;
+    margin-right: 0.875em;
 
-    height: 26px ;
-    width: 6px;
+    height: 1.625em ;
+    width: 0.375em;
 }
 .menu{
     font-family: "Roboto medium";
@@ -77,10 +77,10 @@ const Conteudo = styled.div`
     display:flex;
     flex-direction: row;
     width: 100%;
-    padding: 15px 21px 17px 21px;
+    padding: 1em 1.25em;
 
     background-color: white;
-    box-shadow: 0px 0px 4px 1px rgba(186, 186, 186, 0.25);
+    box-shadow: 0px 0px 0.25em 0.0625em rgba(186, 186, 186, 0.25);
 }
 
 .usuario{
@@ -95,17 +95,17 @@ const Conteudo = styled.div`
     flex-direction: row-reverse;
 
     background-image: url("./assets/images/account_avatar_face_man_people_profile_user_icon_123197.svg");
-    height: 57px;
-    width: 57px;
+    height: 3.5em;
+    width: 3.5em;
 }
 
 .novo-aluno, .alunos{
     background-color: white;
-    box-shadow: 0px 0px 4px 1px rgba(186, 186, 186, 0.25);
+    box-shadow: 0px 0px 0.25em 0.0625em rgba(186, 186, 186, 0.25);
 }
 
 .alunos{
-    margin-top: 32px;
+    margin-top: 2em;
 }
 .notificacao{
     font-family: "Roboto bold";
@@ -156,7 +156,7 @@ const Conteudo = styled.div`
     display: flex;
     flex-direction: column;
 
-    padding: 33px 0px 38px 0px;
+    padding: 2em 0px 2.3em 0px;
     font-family: "Roboto regular";
 }
 .form{
@@ -171,7 +171,7 @@ const Conteudo = styled.div`
     flex-direction: row;
     align-items: center;
 
-    font-size: 32px;
+    font-size: 2em;
     padding-bottom: 30px;
     align-self: flex-start;
     padding-left: 42px;
@@ -179,8 +179,8 @@ const Conteudo = styled.div`
 .novo-aluno button{
     font-family: "Roboto bold";
     background:#E911C6;
-    width: 106px;
-    height: 36px;
+    width: 6.625em;
+    height: 2.25em;
     border-radius: 44px;
     color: white;
     border: none;
@@ -195,30 +195,30 @@ const Conteudo = styled.div`
 .label{
     display: flex;
     align-items: center;
-    margin-left: 67px;
+    margin-left: 4.1em;
 }
 .labelCurso{
     display: flex;
     align-items: center;
-    margin-left: 71px;
+    margin-left: 4.4em;
 }
 .labelCham{
     display: flex;
     align-items: center;
-    margin-left: 42px;
+    margin-left: 2.625em;
 }
 
 .info input{
     height: 25px;
     width: 190px;
-    border-radius: 4px;
+    border-radius: 0.25em;
     border: 1px solid #A8A8A8;
 
     margin: 7.5px 11px;
 }
 
 .interac{
-    padding-top: 32px;
+    padding-top: 2em;
     padding-left: 56px;
 }
 
@@ -226,8 +226,9 @@ const Conteudo = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
+    font-family: "Roboto bold";
 
-    font-size: 32px;
+    font-size: 2em;
     padding-bottom: 30px;
 }
 
@@ -241,19 +242,21 @@ const Conteudo = styled.div`
     height: 67px;
 }
 th{
+    font-size: 1.125em;
     text-align:left;
     color: white;
 }
 th{
-    padding-left: 47px;
+    padding-left: 37px;
 }
 .tabela-alunos table{
     width: 100%;
     border-collapse: collapse;
 }
 td{
-    padding: 20px 0px 20px 47px;
-    font-family: "Roboto regular";
+    padding: 20px 0px 20px 37px;
+    font-family: "Roboto medium";
+    font-size: 1.125em;
 }
 tr:nth-child(odd){
     background-color: #f5f5f5;
